ca596bc6b1ba8112b5e256c7f36d0f39a70d4f00
Benny Hoang <benny.hoang717127@gmail.com>
2026-07-30 23:46:46 -0600

    Finished LLM file

--- server/modules/llm.py
+++ server/modules/llm.py
@@ -7,4 +7,16 @@ load_dotenv()
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 def get_lim_chain(vector_store):
-    pass
+    llm = ChatGroq(
+        groq_api_key=GROQ_API_KEY,
+        model_name="llama3-70b-8192"
+    )
+
+    retriever = vector_store.as_retriever(search_kwargs={"k": 3})
+
+    return RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=retriever,
+        return_source_documents=True
+    )
