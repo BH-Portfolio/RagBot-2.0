2eedee972a3a0c046e84fb89eeb705f247d17539
Benny Hoang <benny.hoang717127@gmail.com>
2026-08-04 21:55:20 -0600

    Fixed bug with path name being wrong

--- client/utils/api.py
+++ client/utils/api.py
@@ -5,8 +5,8 @@ def upload_pdfs_api(files):
     files_payload = [
         ("files", (f.name, f.read(), "application/pdf")) for f in files
     ]
-    return requests.post(f"{API_URL}/uploads_pdfs/", files=files_payload)
+    return requests.post(f"{API_URL}/upload_pdfs/", files=files_payload)
 
 
 def ask_question_api(question):
-    return requests.post(f"{API_URL}/ask", params={"quesiton": question})
+    return requests.post(f"{API_URL}/ask", params={"question": question})
